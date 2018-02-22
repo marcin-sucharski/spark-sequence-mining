141e4eef768addd4d9d3af07037ec701a1d56540
Marcin Sucharski <marcin.sucharski.github@gmail.com>
2018-02-22 23:31:55 +0100

    Cleanup CollectionDistinctUtils code

--- spark-impl/src/main/scala/one/off_by/sequence/mining/gsp/utils/CollectionDistinctUtils.scala
+++ spark-impl/src/main/scala/one/off_by/sequence/mining/gsp/utils/CollectionDistinctUtils.scala
@@ -1,67 +1,89 @@
 package one.off_by.sequence.mining.gsp.utils
 
 import scala.reflect.ClassTag
-import scala.util.control.Breaks._
 
 object CollectionDistinctUtils {
   implicit class IteratorDistinct[T >: Null](iterator: Iterator[T])(implicit ct: ClassTag[T]) {
     def distinct: Iterator[T] = {
-      var size = baseArraySize
+      val state = prepareInitialArraysSet()
+
+      insertItemsIntoArraysSet(state)
+      makeIterator(state)
+    }
+
+    @inline
+    private[this] final def prepareInitialArraysSet(): ArraySetState[T] = {
       val set = new Array[Array[T]](maxSetLevels)
-      set(0) = new Array[T](size)
-      var top = 0
+      set(0) = new Array[T](baseArraySize)
+      ArraySetState(set, baseArraySize, 0)
+    }
+
+    @inline
+    private[this] final def insertItemsIntoArraysSet(state: ArraySetState[T]): Unit = {
+      type SuccessfullyInserted = Boolean
+
+      @inline
+      def tryInsertItem(item: T, hashCode: Int): SuccessfullyInserted = {
+        var current = state.topIndex
+
+        while (current >= 0) {
+          val array = state.set(current)
+          val index = hashCode & (array.length - 1)
+          if (array(index) == null || array(index) == item) {
+            array(index) = item
+            return true
+          } else {
+            current -= 1
+          }
+        }
+
+        false
+      }
+
+      @inline
+      def extendArraySet(item: T, hashCode: Int): Unit = {
+        state.lastLayerSize *= sizeMultiplier
+        val nextArray = new Array[T](state.lastLayerSize)
+        nextArray(hashCode & (state.lastLayerSize - 1)) = item
+        state.topIndex += 1
+        state.set(state.topIndex) = nextArray
+      }
 
       while (iterator.hasNext) {
         val item = iterator.next()
         val hashCode = item.hashCode()
 
-        var current = top
-        breakable {
-          while (current >= 0) {
-            val array = set(current)
-            val index = hashCode & (array.length - 1)
-            if (array(index) == null || array(index) == item) {
-              array(index) = item
-              break
-            } else {
-              current -= 1
-            }
-          }
-        }
-
-        if (current < 0) {
-          size *= sizeMultiplier
-          val nextArray = new Array[T](size)
-          nextArray(hashCode & (size - 1)) = item
-          top += 1
-          set(top) = nextArray
+        if (!tryInsertItem(item, hashCode)) {
+          extendArraySet(item, hashCode)
         }
       }
+    }
 
-
+    @inline
+    private[this] final def makeIterator(state: ArraySetState[T]): Iterator[T] =
       new Iterator[T] {
         private[this] var nextIndex = 0
 
         override def hasNext: Boolean = {
-          while (top >= 0 && set(top)(nextIndex) == null) {
+          while (state.topIndex >= 0 && state.set(state.topIndex)(nextIndex) == null) {
             if (!findNextInArray()) {
-              set(top) = null
-              top -= 1
+              state.set(state.topIndex) = null
+              state.topIndex -= 1
               nextIndex = 0
             }
           }
 
-          top >= 0 && set(top)(nextIndex) != null
+          state.topIndex >= 0 && state.set(state.topIndex)(nextIndex) != null
         }
 
         override def next(): T =
           if (hasNext) {
-            val item = set(top)(nextIndex)
-            var current = top
+            val item = state.set(state.topIndex)(nextIndex)
+            var current = state.topIndex
             while (current >= 0) {
-              val index = nextIndex & (set(current).length - 1)
-              if (set(current)(index) == item) {
-                set(current)(index) = null
+              val index = nextIndex & (state.set(current).length - 1)
+              if (state.set(current)(index) == item) {
+                state.set(current)(index) = null
               }
               current -= 1
             }
@@ -69,15 +91,20 @@ object CollectionDistinctUtils {
           } else Iterator.empty.next()
 
         private def findNextInArray(): Boolean = {
-          val array = set(top)
+          val array = state.set(state.topIndex)
           while (nextIndex < array.length && array(nextIndex) == null) {
             nextIndex += 1
           }
           nextIndex < array.length
         }
       }
-    }
   }
+
+  private[this] final case class ArraySetState[T](
+    set: Array[Array[T]],
+    var lastLayerSize: Int,
+    var topIndex: Int
+  )
 
   private[this] val maxSetLevels = 16
   private[this] val sizeMultiplier = 2
